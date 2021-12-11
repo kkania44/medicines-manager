1fd8424f60a9c43ab8919e078d4c9effd5227375
Karol Wojtachnia <50627253+KarolWojtachnia@users.noreply.github.com>
2021-12-11 16:20:33 +0100

    Some additions

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -24,6 +26,10 @@ export class LoginComponent implements OnInit {
 
   login() {
     alert('Logowanie');
+  }
+
+  goToRegister() {
+    this.router.navigateByUrl('rejestracja');
   }
 
 }
